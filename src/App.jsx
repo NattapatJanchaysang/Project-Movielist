0119e183e159779c3f21ee5c3db6046631c6435f
Nattapat <66011360@kmitl.ac.th>
2025-11-19 14:18:27 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Moviecard from './components/Moviecard'
+
+function App() {
+
+  return (
+    <>
+      
+      <Moviecard movie={{title: "Tim's Film", release_date: "2024"}} />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home(){
+    const movies = [
+        {id: 1, title: "พี่มาก..พระโขนง", release_date: "2556"}
+        {id: 2, title: "สัปเหร่อ", release_date: "2566"}
+        {id: 3, title: "สุริโยไท", release_date: "2544"}
+        {id: 4, title: "นาคี 2", release_date: "2561"}
+    ]
+
+    return <div className="home">
+        <div></div>
+    </div>
+
+}
